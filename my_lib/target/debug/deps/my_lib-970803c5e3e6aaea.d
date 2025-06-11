c9151ce3cea4f002943893d292a0aa78af66edce
khrore <super.stas.0978@gmail.com>
2025-06-11 07:52:26 +0000

    deleted target lib

--- my_lib/target/debug/deps/my_lib-970803c5e3e6aaea.d
+++
@@ -1,5 +0,0 @@
-/home/user/web/rust-server/my_lib/target/debug/deps/libmy_lib-970803c5e3e6aaea.rmeta: src/lib.rs
-
-/home/user/web/rust-server/my_lib/target/debug/deps/my_lib-970803c5e3e6aaea.d: src/lib.rs
-
-src/lib.rs:
